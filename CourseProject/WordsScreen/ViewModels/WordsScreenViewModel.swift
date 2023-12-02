1f14f0aacb8aa62370d8e83559103186e274dd9e
Nikita <volodko.nikita.1979@gmail.com>
2023-12-02 14:55:22 +0300

    Add func "alphabet" for "Word" type

---
+++ CourseProject/WordsScreen/ViewModels/WordsScreenViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WordsScreenViewModel.swift
+//  CourseProject
+//
+//  Created by admin on 2.12.23.
+//
+
+import Foundation
---
+++ CourseProject/WordsScreen/WordsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  WordScreen.swift
+//  CourseProject
+//
+//  Created by admin on 2.12.23.
+//
+
+import SwiftUI
+
+struct WordScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WordScreen()
+}
